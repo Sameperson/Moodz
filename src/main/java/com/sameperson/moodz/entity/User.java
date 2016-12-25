fdb975295405dfd3b838ca94c8e4df00e17ac282
Saem <ottosigma@gmail.com>
2016-12-25 20:50:19 +0200

    User and Role entities

---
+++ src/main/java/com/sameperson/moodz/entity/User.java
@@ -0,0 +1,97 @@
+package com.sameperson.moodz.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @Size(min = 1, max = 16)
+    private String username;
+
+    @Column(length = 24)
+    @Size(min = 6, max = 24)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+    @Lob
+    private byte[] userpic;
+
+    @OneToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.getName()));
+        return authorities;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public byte[] getUserpic() {
+        return userpic;
+    }
+
+    public void setUserpic(byte[] userpic) {
+        this.userpic = userpic;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
