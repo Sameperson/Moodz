2a6e215e14171061fe85ea3c05a2a7cb2dbf65e4
Saem <ottosigma@gmail.com>
2017-01-12 23:35:35 +0200

    basic login page and controller

--- src/main/java/com/sameperson/moodz/controller/LoginController.java
+++ src/main/java/com/sameperson/moodz/controller/LoginController.java
@@ -1,13 +1,24 @@
 package com.sameperson.moodz.controller;
 
+import com.sameperson.moodz.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    public String login() {
+    public String login(Model model, HttpServletRequest request) {
+        model.addAttribute("user", new User());
+        try {
+            Object flash = request.getSession().getAttribute("flash");
+            model.addAttribute("flash", flash);
+            request.getSession().removeAttribute("flash");
+        } catch (Exception ex) {}
+        model.addAttribute("heading","Log in");
         return "login";
     }
 
